ed509386f52dceb9f395a55ca75c1aca5a3755ee
LaislaSte <stephenielaisla@gmail.com>
2024-11-08 14:03:27 -0300

    refactor: changing Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,60 +4,53 @@ import * as S from './styles';
 
 export default function Footer() {
 	return (
-		<S.ContainerFooter className="bg-[#F4F4F4] px-5 pb-7 pt-16">
-			<div className="flex items-end justify-between">
-				<div>
-					<p className="mb-2 text-center text-base font-bold text-white">
-						Navegue pelo Site
+		<S.ContainerFooter className="flex h-[25vh] items-end bg-[#F4f4f4] pb-10">
+			<div className="flex w-full flex-wrap items-start justify-evenly ">
+				<Link href="/eventos">
+					<p className="text-md text-center font-bold text-white">
+						Página Inicial
 					</p>
-					<div className="flex items-center justify-center gap-5">
-						<div className="flex flex-col gap-1">
-							<Link href="/eventos">HOME</Link>
-							<p className="text-center text-sm font-normal text-white">
-								Página Inicial
-							</p>
-							<p className="text-center text-sm font-normal text-white">
-								Eventos Inscritos
-							</p>
-							<p className="text-center text-sm font-normal text-white">
-								Criar Evento
-							</p>
-							<p className="text-center text-sm font-normal text-white">
-								Meus Eventos
-							</p>
-						</div>
-						<div className="flex flex-col gap-1">
-							<p className="text-center text-sm font-normal text-white">
-								Minhas Avaliações
-							</p>
-						</div>
-					</div>
-				</div>
-				<div>
-					<div>
-						<p className="mb-2 text-center text-base font-bold text-white">
-							Navegue pelo Engetec
-						</p>
-						<div className="flex items-center justify-center gap-5">
-							<div className="flex flex-col gap-1">
-								<p className="text-center text-sm font-normal text-white">
-									Página Inicial
-								</p>
-							</div>
-							<div className="flex flex-col gap-1">
-								<p className="text-center text-sm font-normal text-white">
-									Menu para Submissão
-								</p>
-								<p className="text-center text-sm font-normal text-white">
-									Salas e Atividades
-								</p>
-								<p className="text-center text-sm font-normal text-white">
-									Anais Anteriores
-								</p>
-							</div>
-						</div>
-					</div>
-				</div>
+				</Link>
+				{/* <Link href="/">
+					<p className="text-center text-md font-bold text-white">
+						Eventos Inscritos
+					</p>
+				</Link> */}
+				<Link href="/criar-evento">
+					<p className="text-md text-center font-bold text-white">
+						Criar Evento
+					</p>
+				</Link>
+				<Link href="/dashboard/meus-eventos-criados">
+					<p className="text-md text-center font-bold text-white">
+						Meus Eventos Criados
+					</p>
+				</Link>
+				<Link href="/dashboard/avaliar-artigo">
+					<p className="text-md text-center font-bold text-white">
+						Minhas Avaliações
+					</p>
+				</Link>
+				<Link href="/dashboard/meus-arquivos">
+					<p className="text-md text-center font-bold text-white">
+						Minhas Submissões
+					</p>
+				</Link>
+				<Link href="/dashboard/gerenciamento-avaliacoes">
+					<p className="text-md text-center font-bold text-white">
+						Gerenciamento de Avaliações
+					</p>
+				</Link>
+				<Link href="/eventos">
+					<p className="text-md text-center font-bold text-white">
+						Menu para Submissão
+					</p>
+				</Link>
+				<Link href="/eventos">
+					<p className="text-md text-center font-bold text-white">
+						Anais Anteriores
+					</p>
+				</Link>
 			</div>
 		</S.ContainerFooter>
 	);
